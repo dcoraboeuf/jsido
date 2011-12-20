a731377e1abf7b91e2bad2fa82536173cc7dc0ce
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2011-12-20 21:34:59 +0100

    Correction of unit tests by fixing EOL of Sidol files to LF

--- sido-parser/src/test/java/net/sf/sido/parser/ParserTest.java
+++ sido-parser/src/test/java/net/sf/sido/parser/ParserTest.java
@@ -73,7 +73,7 @@ public class ParserTest extends AbstractParserTest {
 			String message = ex.getLocalizedMessage(strings, Locale.ENGLISH);
 			System.out.format("Parsing error is:%n%s%n", message);
 			assertEquals(
-					"Error while parsing \"parsing-error-0\":  - Input \"<\" (position 32 to 33) is not valid for:\n"
+					"Error while parsing \"parsing-error-0\":  - Input \"<\" (position 30 to 31) is not valid for:\n"
 							+ " - schema/prefix_list/Sequence/prefix/whitespaces/whitespace\n"
 							+ " - schema/prefix_list/Sequence/prefix/\"for\"/'f'\n"
 							+ ".\n",
