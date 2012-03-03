308aee141ee44d431de88cf4ecc93df4d5f749ea
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-03-03 18:22:29 +0100

    #5 Moved the JavaFX generation to the 'javafx' branch

--- sido-javafx-support/src/main/java/net/sf/sido/javafx/ObservableIndexedList.java
+++
@@ -1,8 +0,0 @@
-package net.sf.sido.javafx;
-
-import javafx.collections.ObservableList;
-import net.sf.sido.collections.IndexedList;
-
-public interface ObservableIndexedList<T, K> extends IndexedList<T, K>, ObservableList<T> {
-
-}
